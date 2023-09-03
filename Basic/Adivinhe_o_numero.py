317338925b92701276529a388e77974bd4171a83
Thiago0029 <ti.thiagoporto@gmail.com>
2023-09-03 18:27:30 -0300

    acrescentado comentario if

--- Basic/Adivinhe_o_numero.py
+++ Basic/Adivinhe_o_numero.py
@@ -5,6 +5,10 @@ import random
 
 print("Adivinhe o número")
 
-num = random.random()* 1000
+num = random.random(0, 10)
 
+#n = input("Digite um número\n")
 print(num)
+
+#if (n < num){
+#   print("Jutou alto")
